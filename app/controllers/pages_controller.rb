23faa20de190d82ae1c04490b391bae4076e940a
jeffrothwell <rothwelljeff@gmail.com>
2018-01-10 14:39:43 -0500

    add @header to controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
-
+    @header = "This is the header instance variable"
   end
 
   def about
-
+    @header = "This is the header instance variable"
   end
 
   def contest
-
+    @header = "This is the header instance variable"
   end
 
 end
